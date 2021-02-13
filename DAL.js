fc76c35f9fa83c2c646b1ea0e9d81b159fc0fc3a
Łukasz Adamczyk <lukasz012798@gmail.com>
2021-02-13 18:49:44 +0100

    13 lutego

--- DAL.js
+++ DAL.js
@@ -33,5 +33,45 @@ const postParking = (callback, body) => {
   });
 };
 
+const putParking = (callback, body) => {
+  const { id, name, latitude, longitude, type, side, exit } = body;
+  Parking.findOneAndUpdate(
+    {
+      _id: id,
+    },
+    {
+      $set: {
+        name: name,
+        latitude: latitude,
+        longitude: longitude,
+        type: type,
+        side: side,
+        exit: exit,
+      },
+    },
+    {
+      new: true,
+    },
+    (err, doc) => {
+      if (err) {
+        callback("error", 500);
+      }
+      callback("updated", 201);
+    }
+  );
+};
+
+const deleteParking = (callback, id) => {
+  Parking.findByIdAndDelete(id, (err) => {
+    if (err) {
+      callback(err, 500);
+      return;
+    }
+    callback("_deleted", 200);
+  });
+};
+
 module.exports.getParking = getParking;
 module.exports.postParking = postParking;
+module.exports.putParking = putParking;
+module.exports.deleteParking = deleteParking;
--- routes/api.js
+++ routes/api.js
@@ -2,14 +2,27 @@ const express = require("express");
 const router = express.Router();
 const DAL = require("./../DAL");
 
-// pobieranie wszystkich parkingow
+// pobieranie wszystkich obiektów
 router.get("/", (req, res) => {
   DAL.getParking((data) => res.json(data));
 });
 
+// dodawanie nowego obiektu
 router.post("/", (req, res) => {
   DAL.postParking((data, status) => res.status(status).json(data), req.body);
-  // res.json(req.body)
+});
+
+// aktualizacja obiektu na podstawie podanego id
+router.put("/", (req, res) => {
+  DAL.putParking((data, status) => res.status(status).json(data), req.body);
+});
+
+// usuwanie obiektu na podstawie podanego id
+router.delete("/:id", (req, res) => {
+  DAL.deleteParking(
+    (data, status) => res.status(status).json(data),
+    req.params.id
+  );
 });
 
 module.exports = router;
